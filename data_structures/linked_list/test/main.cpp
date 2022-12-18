54e0e46dc1e011aac61b27be7acf4ab851819d6c
Vasilis CodeX <vasilis.tris@gmail.com>
2022-12-18 13:11:22 +0200

    testing environment and shared library

---
+++ data_structures/linked_list/test/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // LinkedList<int> *lli = new SinglyLinkedList<int>();
+    // lli->add(2);
+    // lli->add(3);
+    // lli->add(4);
+    // lli->display();
+
+
+    printf("Helloo!!");
+    return 0;
+}
